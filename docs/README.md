9740fb8174163faf03873ad3e6cbf005e2f701e3
ctholho <valentinbeyer@gmail.com>
2023-05-08 01:14:47 +0200

    docs: init

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+# Docus Starter
+
+Docus is a Nuxt 3 template for markdown pages.
+
+Starter template for [Docus](https://docus.dev).
+For a detailed explanation of how things work, check out [Docus](https://docus.dev).
---
+++ docs/app.config.ts
@@ -0,0 +1,25 @@
+export default defineAppConfig({
+  docus: {
+    title: 'GDPR',
+    description: 'Wrangle your Directus permissions with ease',
+    url: 'https://akronym.io',
+    socials: {
+      github: 'akronym-org/gdpr'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: false
+    },
+    footer: {
+      textLinks: [
+        {
+          text: 'akronym.io',
+          href: 'https://akronym.io'
+        }
+      ],
+    }
+  }
+})
